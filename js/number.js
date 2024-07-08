484e8903dff8f9f64ca0933985b1f0cbabeb316f
GabrielhrZ <gabrielalexanderhr@gmail.com>
2024-07-08 21:08:01 +0100

    Repaso de javascript.

---
+++ js/number.js
@@ -0,0 +1,23 @@
+//1- Tipo Entero y decimal
+const entero = 123;
+const decimal = 3.14;
+
+//2- Notacion cientifica
+const cientifico = 5e3;
+
+//3- Numeros infinitos y NaN
+const infinito = Infinity;
+const noNumero = NaN;
+
+//1-Operaciones aritmeticas
+const suma = 3+4;
+const resta = 4-4;
+const multiplicacion = 3*3;
+const division = 16/ 2;
+
+//2-Modulo y Exponenciacion
+const modulo = 15 % 8;
+const exponenciacion = 2**3;
+
+
+
